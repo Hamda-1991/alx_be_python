eea151f24c6a1ad46fcd6595f1607c57e6dca423
Hamda Hassan <hassanodowa7@gmail.com>
2025-06-20 20:55:38 +0300

    add

---
+++ control-flow/guess_the_number_game.py
@@ -0,0 +1,33 @@
+import random
+
+def play_game():
+    secret_number = random.randint(1, 10)
+    guess_count = 0
+    print("I'm thinking of a number between 1 and 10. Can you guess it?")
+
+    while True:
+        try:
+            guess = int(input("Your guess: "))
+            guess_count += 1
+
+            match guess:
+                case _ if guess == secret_number:
+                    print(f"🎉 Congratulations, you guessed it in {guess_count} tries!")
+                    break
+                case _ if guess > secret_number:
+                    print("📈 Oops, your guess is a bit high. Try again!")
+                case _ if guess < secret_number:
+                    print("📉 Nope, your guess is a bit low. Give it another shot!")
+        except ValueError:
+            print("Please enter a valid number!")
+
+def main():
+    while True:
+        play_game()
+        play_again = input("Play again? (yes/no): ").strip().lower()
+        if play_again != "yes":
+            print("Thanks for playing! Goodbye! 👋")
+            break
+
+if __name__ == "__main__":
+    main()
